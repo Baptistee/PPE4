21c4e1ab24f3f9b96ecc47bac4307a9089e2aada
Baptiste Blct <baptistee@baptiste.blct@gmail.com>
2019-03-08 16:13:56 +0100

    init menu

--- PPE4/Form1.Designer.cs
+++ PPE4/Form1.Designer.cs
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.campagneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vIPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.messageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.artisteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.évenementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consulterToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créerToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.campagneToolStripMenuItem,
+            this.évenementToolStripMenuItem,
+            this.agenceToolStripMenuItem,
+            this.artisteToolStripMenuItem,
+            this.vIPToolStripMenuItem,
+            this.messageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(572, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // campagneToolStripMenuItem
+            // 
+            this.campagneToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem,
+            this.créerToolStripMenuItem});
+            this.campagneToolStripMenuItem.Name = "campagneToolStripMenuItem";
+            this.campagneToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.campagneToolStripMenuItem.Text = "Campagne";
+            // 
+            // agenceToolStripMenuItem
+            // 
+            this.agenceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem2,
+            this.créerToolStripMenuItem2});
+            this.agenceToolStripMenuItem.Name = "agenceToolStripMenuItem";
+            this.agenceToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.agenceToolStripMenuItem.Text = "Agence";
+            // 
+            // vIPToolStripMenuItem
+            // 
+            this.vIPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem4,
+            this.créerToolStripMenuItem4});
+            this.vIPToolStripMenuItem.Name = "vIPToolStripMenuItem";
+            this.vIPToolStripMenuItem.Size = new System.Drawing.Size(36, 20);
+            this.vIPToolStripMenuItem.Text = "VIP";
+            // 
+            // messageToolStripMenuItem
+            // 
+            this.messageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem5,
+            this.créerToolStripMenuItem5});
+            this.messageToolStripMenuItem.Name = "messageToolStripMenuItem";
+            this.messageToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.messageToolStripMenuItem.Text = "Message";
+            // 
+            // artisteToolStripMenuItem
+            // 
+            this.artisteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem3,
+            this.créerToolStripMenuItem3});
+            this.artisteToolStripMenuItem.Name = "artisteToolStripMenuItem";
+            this.artisteToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.artisteToolStripMenuItem.Text = "Artiste";
+            // 
+            // évenementToolStripMenuItem
+            // 
+            this.évenementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consulterToolStripMenuItem1,
+            this.créerToolStripMenuItem1});
+            this.évenementToolStripMenuItem.Name = "évenementToolStripMenuItem";
+            this.évenementToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.évenementToolStripMenuItem.Text = "Événement";
+            this.évenementToolStripMenuItem.Click += new System.EventHandler(this.évenementToolStripMenuItem_Click);
+            // 
+            // consulterToolStripMenuItem
+            // 
+            this.consulterToolStripMenuItem.Name = "consulterToolStripMenuItem";
+            this.consulterToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem.Text = "Consulter";
+            // 
+            // créerToolStripMenuItem
+            // 
+            this.créerToolStripMenuItem.Name = "créerToolStripMenuItem";
+            this.créerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem.Text = "Créer";
+            // 
+            // créerToolStripMenuItem1
+            // 
+            this.créerToolStripMenuItem1.Name = "créerToolStripMenuItem1";
+            this.créerToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem1.Text = "Créer";
+            // 
+            // consulterToolStripMenuItem1
+            // 
+            this.consulterToolStripMenuItem1.Name = "consulterToolStripMenuItem1";
+            this.consulterToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem1.Text = "Consulter";
+            // 
+            // consulterToolStripMenuItem2
+            // 
+            this.consulterToolStripMenuItem2.Name = "consulterToolStripMenuItem2";
+            this.consulterToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem2.Text = "Consulter";
+            // 
+            // créerToolStripMenuItem2
+            // 
+            this.créerToolStripMenuItem2.Name = "créerToolStripMenuItem2";
+            this.créerToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem2.Text = "Créer";
+            // 
+            // consulterToolStripMenuItem3
+            // 
+            this.consulterToolStripMenuItem3.Name = "consulterToolStripMenuItem3";
+            this.consulterToolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem3.Text = "Consulter";
+            // 
+            // créerToolStripMenuItem3
+            // 
+            this.créerToolStripMenuItem3.Name = "créerToolStripMenuItem3";
+            this.créerToolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem3.Text = "Créer";
+            // 
+            // consulterToolStripMenuItem4
+            // 
+            this.consulterToolStripMenuItem4.Name = "consulterToolStripMenuItem4";
+            this.consulterToolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem4.Text = "Consulter";
+            // 
+            // créerToolStripMenuItem4
+            // 
+            this.créerToolStripMenuItem4.Name = "créerToolStripMenuItem4";
+            this.créerToolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem4.Text = "Créer";
+            // 
+            // consulterToolStripMenuItem5
+            // 
+            this.consulterToolStripMenuItem5.Name = "consulterToolStripMenuItem5";
+            this.consulterToolStripMenuItem5.Size = new System.Drawing.Size(152, 22);
+            this.consulterToolStripMenuItem5.Text = "Consulter";
+            // 
+            // créerToolStripMenuItem5
+            // 
+            this.créerToolStripMenuItem5.Name = "créerToolStripMenuItem5";
+            this.créerToolStripMenuItem5.Size = new System.Drawing.Size(152, 22);
+            this.créerToolStripMenuItem5.Text = "Créer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 316);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem campagneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agenceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vIPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem messageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem artisteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem évenementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem consulterToolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem créerToolStripMenuItem5;
     }
 }
 
--- PPE4/Form1.cs
+++ PPE4/Form1.cs
@@ -16,5 +16,10 @@ namespace PPE4
         {
             InitializeComponent();
         }
+
+        private void évenementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
