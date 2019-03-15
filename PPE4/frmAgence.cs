1f92687ac707e593b2796eed49c27227db6c0ee8
Maco13 <over.maco13@gmail.com>
2019-03-15 14:10:16 +0100

    Ajout Classe & form
    classe complète, avec accesseurs et fonction getAllInfo(). Form vide

---
+++ PPE4/frmAgence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE4
+{
+    public partial class Agence : Form
+    {
+        public Agence()
+        {
+            InitializeComponent();
+        }
+
+        private void créeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
